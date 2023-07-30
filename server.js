84e44bf7fa3f9e2a98dc468c67ab551997cb494e
Adarsh <adarsh.khalique@gmail.com>
2023-07-31 00:02:41 +1000

    task 3

--- server.js
+++ server.js
@@ -2,38 +2,13 @@ let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
 
+app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/'));
 
 app.get('/', (req, res)=>{
-    res.render('index.html');
+    res.render('pages/index.ejs');
 });
 
-app.get('/addNumbers',(req,res)=>{
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let sum = parseInt(num1) + parseInt(num2);
-    let obj = {message:'Result is', data:sum}
-
-    res.json(obj);
-});
-
-app.get('/subtractNumbers',(req,res)=>{
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let sum = parseInt(num1) - parseInt(num2);
-    let obj = {message:'Result is', data:sum}
-
-    res.json(obj);
-});
-
-app.get('/multiplyNumbers',(req,res)=>{
-    let num1 = req.query.num1;
-    let num2 = req.query.num2;
-    let sum = parseInt(num1) * parseInt(num2);
-    let obj = {message:'Result is', data:sum}
-
-    res.json(obj);
-});
 
 app.listen(port, ()=>{
     console.log('server is ON');
